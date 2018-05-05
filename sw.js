b24ee32ee6d243dabbb7c92a8fcdebb82aa9434d
bruincub <xiongmaocub@gmail.com>
2018-05-05 14:06:59 -0400

    feat: Add offline first caching
    Cache dependent resources
    Cache visited page resources

---
+++ sw.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const staticCacheName = "mws-restaurant-cache-v1";
+const allCaches = [
+    staticCacheName
+];
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        // Cache resources for offline first
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.addAll([
+                "index.html",
+                "js/dbhelper.js",
+                "js/main.js",
+                "js/restaurant_info.js",
+                "css/styles.css"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        // Remove old caches
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith("mws-restaurant-") &&
+                        !allCaches.includes(cacheName);
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    // Return matched cache resource
+    // otherwise fetch from network and store in cache
+    event.respondWith(
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.match(event.request).then(function(response) {
+                return response || fetch(event.request).then(function(response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
+
