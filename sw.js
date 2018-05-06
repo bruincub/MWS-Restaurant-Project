90cd35b6817ae3fb2d4e35fdd3de99a23721079e
bruincub <xiongmaocub@gmail.com>
2018-05-05 20:01:50 -0400

    fix: Cache all resources and handle root URL path

--- sw.js
+++ sw.js
@@ -11,16 +11,39 @@ self.addEventListener('install', function(event) {
         caches.open(staticCache).then(function(cache) {
             return cache.addAll([
                 'index.html',
+                'restaurant.html',
+                'js/common.js',
                 'js/dbhelper.js',
                 'js/main.js',
                 'js/restaurant_info.js',
-                'css/styles.css'
+                'css/styles.css',
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg',
+                'img/5.jpg',
+                'img/6.jpg',
+                'img/7.jpg',
+                'img/8.jpg',
+                'img/9.jpg',
+                'img/10.jpg',
+                'data/restaurants.json'
             ]);
         })
     );
 });
 
 self.addEventListener('fetch', function(event) {
+    const requestUrl = new URL(event.request.url);
+
+    // Handle root path
+    if (requestUrl.origin === location.origin) {
+        if (requestUrl.pathname === '/') {
+            event.respondWith(caches.match('index.html'));
+            return;
+        }
+    }
+
     // Return matched cache resource
     // otherwise fetch from network and store in cache
     event.respondWith(
