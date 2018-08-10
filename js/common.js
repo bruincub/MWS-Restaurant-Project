c54d1790c1dc8da8fe1dbfca753187cf6f805234
bruincub <xiongmaocub@gmail.com>
2018-08-10 04:39:42 -0400

    enh: Restaurant favorites and reviews
    Add feature to favorite restaurants while online or offline.
    Add feature to submit restaurant review while online or offline.

--- js/common.js
+++ js/common.js
@@ -14,5 +14,33 @@
 (function init() {
     "use strict";
 
-    // const _dbPromse = WSHelper.openDatabase();
+    const maincontent = document.getElementById("maincontent");
+
+    maincontent.addEventListener("click", function(e) {
+	    let restaurantId;
+	    let favStatus;
+
+    	e.stopPropagation();
+
+    	if (e.target.className.includes("favBtn")) {
+    		restaurantId = e.target.value;
+    		if (e.target.className === "favBtn favorited") {
+    			favStatus = true;
+		    } else {
+    			favStatus = false;
+		    }
+
+    	    WSHelper.setFavorite(restaurantId, favStatus, (msg, success) => {
+    	    	if (success) {
+    	    		if (favStatus) {
+    	    			e.target.className = "favBtn";
+			        } else {
+    	    			e.target.className = "favBtn favorited";
+			        }
+		        } else {
+			        WSHelper.showPopdown(msg);
+		        }
+	        });
+	    }
+    });
 })();
--- js/wshelper.js
+++ js/wshelper.js
@@ -15,10 +15,25 @@ class WSHelper {
         }
 
         return idb.open("mws-restaurants", 1, function(upgradeDb) {
+        	let reviewsStore;
+
             switch (upgradeDb.oldVersion) {
                 case 0:
                     upgradeDb.createObjectStore("restaurants", {
                         keyPath: "id"
+                    });
+
+                    reviewsStore = upgradeDb.createObjectStore("reviews", {
+                    	keyPath: "id"
+                    });
+                    reviewsStore.createIndex("by-restaurant", "restaurant_id");
+
+                    upgradeDb.createObjectStore("offlineReviews", {
+                    	keyPath: "restaurant_id"
+                    });
+
+                    upgradeDb.createObjectStore("offlineFavs", {
+						autoIncrement: true
                     });
             }
         }).then(function(db) {
@@ -81,7 +96,7 @@ class WSHelper {
     static fetchRestaurantById(id, callback) {
         const _dbPromise = WSHelper.openDatabase();
 
-        fetch(WSHelper.WS_URL + `/Restaurants/${id}`).then(function(response) {
+        fetch(WSHelper.WS_URL + `/restaurants/${id}`).then(function(response) {
             if (response.ok) {
                 return _dbPromise.then(function(db) {
                     let restaurant = response.clone().json();
@@ -113,8 +128,115 @@ class WSHelper {
         });
     }
 
-    /**
-     * Fetch restaurants by a cuisine type with proper error handling.
+	/**
+	 * Fetch restaurant review
+	 */
+	static fetchRestaurantReviewById(id, callback) {
+		const _dbPromise = WSHelper.openDatabase();
+
+		fetch(WSHelper.WS_URL + `/reviews/?restaurant_id=${id}`).then(function(response) {
+			if (response.ok) {
+				return _dbPromise.then(function(db) {
+					let reviews = response.clone().json();
+
+					response.json().then(function(r) {
+						let tx = db.transaction("reviews", "readwrite");
+						let store = tx.objectStore("reviews");
+
+						r.forEach(function(review) {
+							store.put(review);
+						});
+					});
+
+					return reviews;
+				});
+			} else {
+				const error = (`Request failed. Returned status of ${xhr.status}`);
+				callback(error, null);
+			}
+		}).then(function(reviews) {
+			callback(null, reviews);
+		}).catch(function(error) {
+			_dbPromise.then(function(db) {
+				let tx = db.transaction("reviews", "readonly");
+				let store = tx.objectStore("reviews");
+				let index = store.index("by-restaurant");
+
+				return index.getAll(parseInt(id));
+			}).then(function(reviews) {
+				callback(null, reviews);
+			});
+		});
+	}
+
+	/**
+	 * Post restaurant review
+	 */
+	static postRestaurantReview(id, callback) {
+		const url = "http://localhost:1337/reviews/";
+		const _dbPromise = WSHelper.openDatabase();
+		let review = JSON.parse(JSON.stringify({
+			restaurant_id: id,
+			name: document.getElementById("name").value,
+			createdAt: Date.now(),
+			modifiedAt: Date.now(),
+			rating: document.getElementById("rating").value,
+			comments: document.getElementById("review").value
+		}));
+
+		if (!navigator.onLine) {
+			_dbPromise.then(function(db) {
+				let tx = db.transaction("offlineReviews", "readwrite");
+				let store = tx.objectStore("offlineReviews");
+
+				store.add(review);
+			});
+
+			window.addEventListener("online", function(e) {
+				"use strict";
+
+				WSHelper.postOfflineReviews();
+			}, {once: true});
+
+			callback("It appears you are offline. Your post will be submitted once you are reconnected.", review);
+		} else {
+			fetch(url, {
+				method: "post",
+				body: JSON.stringify({
+					restaurant_id: id,
+					name: document.getElementById("name").value,
+					createdAt: Date.now(),
+					modifiedAt: Date.now(),
+					rating: document.getElementById("rating").value,
+					comments: document.getElementById("review").value
+				})
+			}).then(function(response) {
+				if (response.ok) {
+					// Store in IndexedDB
+					return _dbPromise.then(function(db) {
+						let tx = db.transaction("reviews", "readwrite");
+						let store = tx.objectStore("reviews");
+						let review = response.clone().json();
+
+						response.json().then(function(r) {
+							store.put(r);
+						});
+
+						return review;
+					});
+				} else {
+					callback("There was a problem submitting your review. Please try again.", null);
+				}
+			}).then(function(review) {
+				callback("New review posted.", review);
+			}).catch(function(error) {
+				callback(error.message + " occurred while posting a review.", null);
+			});
+		}
+	}
+
+	/**
+	 * Fetch restaurants by a cuisine type with proper error handling.
      */
     static fetchRestaurantByCuisine(cuisine, callback) {
         // Fetch all restaurants with proper error handling
@@ -229,4 +351,118 @@ class WSHelper {
         marker.addTo(newMap);
         return marker;
     }
+
+	/**
+	 * Post offline reviews
+	 */
+	static postOfflineReviews(callback) {
+		const _dbPromise = WSHelper.openDatabase();
+
+		_dbPromise.then(function(db) {
+			let tx = db.transaction("offlineReviews", "readwrite");
+			let store = tx.objectStore("offlineReviews");
+
+			store.getAll().then(function(reviews) {
+				reviews.forEach(function(review) {
+					WSHelper.postRestaurantReview(review.restaurant_id, (msg, review) => {
+						WSHelper.showPopdown(msg);
+					});
+				});
+
+			}).then(function() {
+				store.clear();
+			});
+
+		});
+	}
+
+	static showPopdown(content) {
+		"use strict";
+
+		let popdown = document.getElementById("popdown");
+
+		popdown.innerHTML = "<p>" + content + "</p>";
+		popdown.classList.add("show");
+		setTimeout(function() {
+			popdown.classList.remove("show");
+		}, 4000);
+	}
+
+	static setFavorite(id, favStatus, callback) {
+		"use strict";
+
+		let status = !favStatus;
+		const url = "http://localhost:1337/restaurants/" + id + "/?is_favorite=" + status;
+		const _dbPromise = WSHelper.openDatabase();
+
+		if (!navigator.onLine) {
+			_dbPromise.then(function(db) {
+				let tx = db.transaction("offlineFavs", "readwrite");
+				let store = tx.objectStore("offlineFavs");
+
+				let favorite = '{"restaurant_id": ' + id + ', "status": "' + status + '"}';
+
+				store.add(JSON.parse(favorite));
+			});
+
+			window.addEventListener("online", function(e) {
+				"use strict";
+
+				WSHelper.setOfflineFavs();
+			}, {once: true});
+
+			callback("It appears you are offline. Your restaurant favorite will be submitted once you are reconnected.", "false");
+		} else {
+			fetch(url, {
+				method: "put"
+			}).then(function(response) {
+				if (response.ok) {
+					// Update restaurant in IndexedDB
+					_dbPromise.then(function(db) {
+						response.json().then(function(restaurant) {
+							let tx = db.transaction("restaurants", "readwrite");
+							let store = tx.objectStore("restaurants");
+
+							store.put(restaurant);
+						});
+					});
+				} else {
+					callback("There was a problem favoriting the restaurant. Please try again.", "false");
+				}
+			}).then(function() {
+				callback("Restaurant favorite status changed.", "true");
+			}).catch(function(error) {
+				callback(error.message + " occurred while favoriting a restaurant.", "false");
+			});
+		}
+
+	}
+
+	static setOfflineFavs(callback) {
+		const _dbPromise = WSHelper.openDatabase();
+
+		_dbPromise.then(function(db) {
+			let tx = db.transaction("offlineFavs", "readwrite");
+			let store = tx.objectStore("offlineFavs");
+			let status;
+
+			store.getAll().then(function(favs) {
+				favs.forEach(function(fav) {
+					if (fav.status === "true") {
+						status = false;
+					} else {
+						status = true;
+					}
+
+					WSHelper.setFavorite(fav.restaurant_id, status, (msg, success) => {
+						if (success) {
+							WSHelper.showPopdown(msg);
+						}
+					});
+				});
+			}).then(function() {
+				store.clear();
+			});
+		});
+	}
 }
